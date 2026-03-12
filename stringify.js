faa609223570a2bc24738f7a7688d733780140eb
kapkap <167351369+mycapcap@users.noreply.github.com>
2026-03-12 20:19:46 +0000

    Update stringify.js

--- stringify.js
+++ stringify.js
@@ -1,3 +1,5 @@
+//stringify Map & Set for encryption
+
 const jStrgfy = (jsonObj) => {
 
 
@@ -30,39 +32,34 @@ return JSON.stringify(jsonObj, jsonMapSetReplacer)
 
 }
 
+//Parse Map & Set after decryption
 
 const jParse = (jStr) => {
 
-/**
- * Provides a `JSON.parse` reviver function that supports Map and Set object deserialization.
- *
- * Must be used to deserialize JSON data serialized using #jsonMapSetReplacer.
- */
-
-function jsonMapSetReviver (_, value_)
-{
-  if (Array.isArray (value_) && value_.length > 0)
-  {
-    let isMap, isSet;
-    if ((isMap = value_ [0] === '@Map') || (isSet = value_ [0] === '@Set') || value_ [0] === '@Array')
-    {
-      value_.shift ();
-      if (isMap)
-        value_ = new Map (value_);
-      else if (isSet)
-        value_ = new Set (value_);
-    }
-  }
-
-  return value_;
-
-}
-try {
-  return JSON.parse(jStr, jsonMapSetReviver)
-  
-} catch (error) {
-  return false
-}
+        function jsonMapSetReviver (_, value_)
+        {
+          if (Array.isArray (value_) && value_.length > 0)
+          {
+            let isMap, isSet;
+            if ((isMap = value_ [0] === '@Map') || (isSet = value_ [0] === '@Set') || value_ [0] === '@Array')
+            {
+              value_.shift ();
+              if (isMap)
+                value_ = new Map (value_);
+              else if (isSet)
+                value_ = new Set (value_);
+            }
+          }
+        
+          return value_;
+        
+        }
+        try {
+          return JSON.parse(jStr, jsonMapSetReviver)
+          
+        } catch (error) {
+          return false
+        }
 
 
 }
